670a86a771358090c9d473e4a8b1c092cf1f5d6d
M.Faiz <luthfimuladi24@gmail.com>
2024-11-02 11:22:24 +0700

    motion 0.1

--- src/component/layout/navigation.jsx
+++ src/component/layout/navigation.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import menu from "../../assets/svg icon/menu.svg";
+import { motion } from "framer-motion";
 export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const navRef = useRef(null);
   const handleClick = () => {
     setIsOpen(!isOpen);
     console.log(isOpen);
   };
-
   const handleOutsideClick = (event) => {
     if (navRef.current && !navRef.current.contains(event.target)) {
       setIsOpen(false);
@@ -20,58 +21,126 @@ export default function Navigation() {
     };
   }, []);
 
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
+    window.addEventListener("resize", handleResize);
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.5 },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: -100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        duration: 1.5,
+        stiffness: 120,
+      },
+    },
+  };
+
+  const mobileVariants = {
+    closed: { opacity: 1, x: -200 },
+    open: {
+      x: 0,
+      transition: {
+        type: "spring",
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
     <div
       ref={navRef}
       className="fixed p-5 lg:absolute lg:px-20 lg:py-12 lg:flex lg:items-center lg:justify-between lg:w-full font-poppins z-[999]"
     >
-      <div className="flex items-center gap-2 mb-2 lg:mb-0 ">
-        <button
+      <motion.div
+        className="flex items-center gap-2 mb-2 lg:mb-0 "
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.button
           className="p-2 rounded-full lg:hidden bg-darkPrimary hover:bg-darkPrimary/80"
           onClick={handleClick}
+          variants={itemVariants}
         >
           <img src={menu} alt="" className="w-5 " />
-        </button>
+        </motion.button>
 
-        <h1 className="text-xl font-semibold lg:text-2xl text-slate-200">
+        <motion.h1
+          className="text-xl font-semibold lg:text-2xl text-slate-200"
+          variants={itemVariants}
+        >
           <a href="#">Siti Alyana</a>
-        </h1>
-      </div>
+        </motion.h1>
+      </motion.div>
       <nav>
-        <ul
-          className={`gap-5 lg:flex text-slate-200 bg-lightPrimary lg:bg-transparent px-2 py-5 lg:p-0 rounded-xl shadow-xl  ${
+        <motion.ul
+          className={`gap-5 lg:flex lg:flex-row-reverse text-slate-200 bg-lightPrimary lg:bg-transparent px-2 py-5 lg:p-0 rounded-xl shadow-2xl lg:shadow-none border border-darkPrimary lg:border-none  ${
             isOpen ? "flex flex-col" : "hidden"
           } `}
+          variants={isMobile ? mobileVariants : containerVariants}
+          initial={isMobile ? "closed" : "hidden"}
+          animate={isMobile ? (isOpen ? "open" : "closed") : "visible"}
         >
           <NavList
             nama="About"
             href="#service"
             onClick={() => setIsOpen(false)}
+            variants={isMobile ? "" : itemVariants}
           />
           <NavList
             nama="Experience"
             href="#experience"
             onClick={() => setIsOpen(false)}
+            variants={isMobile ? "" : itemVariants}
           />
           <NavList
             nama="Project"
             href="#project"
             onClick={() => setIsOpen(false)}
+            variants={isMobile ? "" : itemVariants}
           />
           <NavList
             nama="Content"
             href="#testimony"
             onClick={() => setIsOpen(false)}
+            variants={isMobile ? "" : itemVariants}
           />
-        </ul>
+        </motion.ul>
       </nav>
     </div>
   );
 }
 
-const NavList = ({ nama, href, onClick }) => {
+const NavList = ({ nama, href, onClick, ...props }) => {
   return (
-    <li>
+    <motion.li
+      {...props}
+      whileHover={{
+        scale: 1.1,
+        transition: { type: "spring", stiffness: 400 },
+      }}
+      whileTap={{
+        scale: 0.9,
+        transition: { type: "spring", stiffness: 400 },
+      }}
+    >
       <a
         onClick={onClick}
         href={href}
@@ -79,6 +148,7 @@ const NavList = ({ nama, href, onClick }) => {
       >
         {nama}
       </a>
-    </li>
+    </motion.li>
   );
 };
+// const MotionNavlist = motion(NavList);
--- src/pages/hero section.jsx
+++ src/pages/hero section.jsx
@@ -5,21 +5,57 @@ import iconYT from "../assets/svg icon/image 6.svg";
 import iconIG from "../assets/svg icon/image 9.svg";
 import iconWA from "../assets/svg icon/image 8.svg";
 import illustBintang from "../assets/svg/Group 1.svg";
-import { motion } from "framer-motion";
+import { delay, motion } from "framer-motion";
 
 const containerVariants = {
   hidden: { opacity: 0 },
   show: {
     opacity: 1,
     transition: {
-      staggerChildren: 0.25,
+      staggerChildren: 0.5,
+      delayChildren: 1,
+      delay: 1,
+    },
+  },
+};
+
+const childVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5,
     },
   },
 };
 
 const itemVariants = {
-  hidden: { opacity: 0 },
-  show: { opacity: 1 },
+  hidden: { opacity: 0, y: 90 },
+  show: {
+    opacity: 1,
+    y: 0,
+    scale: 1.1,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+      damping: 10,
+      duration: 1,
+    },
+  },
+};
+
+const textVariants = {
+  hidden: { opacity: 0, x: -100 },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+      damping: 10,
+      duration: 1,
+    },
+  },
 };
 
 export default function HeroSection() {
@@ -30,52 +66,83 @@ export default function HeroSection() {
     >
       <div className="flex flex-col-reverse items-center py-20 lg:py-0 lg:flex-row lg:justify-center lg:gap-20 lg:pr-10 lg:mt-20">
         {/* ====== kiri start ====== */}
-        <div className="text-center lg:text-start mt-7 lg:mt-0 lg:w-1/3 lg:mr-20">
-          <p className="text-xl font-semibold lg:text-3xl text-darkPrimary">
+        <motion.div
+          variants={containerVariants}
+          initial="hidden"
+          animate="show"
+          className="text-center lg:text-start mt-7 lg:mt-0 lg:w-1/3 lg:mr-20"
+        >
+          <motion.p
+            variants={textVariants}
+            className="text-xl font-semibold lg:text-3xl text-darkPrimary"
+          >
             Hallo, I’m Siti Alyana
-          </p>
-          <h3 className="text-2xl font-semibold text-white lg:mb-5 lg:text-4xl">
+          </motion.p>
+          <motion.h3
+            variants={textVariants}
+            className="text-2xl font-semibold text-white lg:mb-5 lg:text-4xl"
+          >
             UI/UX Designer
-          </h3>
-          <p className="hidden w-full tracking-wide text-white lg:block lg:text-lg">
+          </motion.h3>
+          <motion.p
+            variants={textVariants}
+            className="hidden w-full tracking-wide text-white lg:block lg:text-lg"
+          >
             Saya dari Indonesia tepatnya di Jawa Barat. Saya pernah bersekolah
             SMK di jurusan Multimedia dan pernah membuat beberapa projects. Saya
             suka mendesain dengan tema-tema yang simpel dan menyenangkanya suka
             mendesain dengan tema-tema yang simpel dan menyenangkan
-          </p>
-          <div className="flex flex-col gap-2 mt-5 lg:gap-5 lg:flex-row ">
+          </motion.p>
+          <motion.div
+            variants={childVariants}
+            className="flex flex-col gap-2 mt-5 lg:gap-5 lg:flex-row "
+          >
             {/* ==== button start ==== */}
-            <button className="flex items-center justify-center gap-3 px-5 py-2 text-white duration-300 scale-75 shadow-lg lg:scale-100 bg-darkPrimary hover:bg-darkPrimary/80 hover:shadow-white/10">
+            <motion.button
+              variants={textVariants}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center justify-center gap-3 px-5 py-2 text-white scale-75 shadow-lg lg:scale-100 bg-darkPrimary hover:shadow-white/10"
+            >
               <img src={iconMail} alt="" className="w-5" />
               Email Me
-            </button>
+            </motion.button>
             {/* ==== button end ==== */}
             {/* ==== sosmed start ==== */}
-            <div className="flex items-center justify-center gap-3 lg:ml-5 lg:gap-10">
+            <motion.div
+              variants={textVariants}
+              className="flex items-center justify-center gap-3 lg:ml-5 lg:gap-10"
+            >
               <IconSosmed icon={iconYT} />
               <IconSosmed icon={iconIG} />
               <IconSosmed icon={iconWA} />
-            </div>
-          </div>
-          <div className="mt-5 text-center">
+            </motion.div>
+          </motion.div>
+          <motion.div className="mt-5 text-center">
             <a href="#" className="text-xs text-white hover:underline ">
               Download CV
             </a>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
         {/* ====== kiri end ====== */}
         {/* ====== kanan start ====== */}
-        <motion.div variants={containerVariants}>
+        <motion.div
+          variants={containerVariants}
+          initial="hidden"
+          animate="show"
+        >
           <motion.div className="relative w-40 h-56 mt-8 scale-110 shadow-2xl bg-darkPrimary lg:w-52 lg:h-80 rounded-3xl lg:scale-100">
-            <img
+            <motion.img
               src={heroImg}
               alt=""
-              className="absolute bottom-0 lg:scale-110 lg:bottom-4 left-4"
+              className="absolute bottom-3 w-36 lg:w-96 lg:bottom-4 left-4"
+              variants={itemVariants}
             />
             <motion.img
               src={illustBintang}
               alt=""
-              className="absolute scale-50 lg:scale-75 -bottom-14 -right-20"
+              className="absolute w-20 lg:w-40 -bottom-4 -right-8 lg:-bottom-10 lg:-right-16"
+              variants={itemVariants}
             />
           </motion.div>
         </motion.div>
